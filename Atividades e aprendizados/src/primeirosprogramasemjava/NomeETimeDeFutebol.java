cd5a76a71273221f2d51344511ffb2e88a7d3e0f
Flavia <flaviarenata969@gmail.com>
2021-06-24 19:10:41 -0300

    Aprendendo JAVA

---
+++ Atividades e aprendizados/src/primeirosprogramasemjava/NomeETimeDeFutebol.java
@@ -0,0 +1,18 @@
+/*
+ * Programa para dizer qual meu nome e meu time
+ * Criado por Flavia
+ */
+
+package primeirosprogramasemjava;
+
+public class NomeETimeDeFutebol {
+	public static void main(String[] args) {
+		
+		String nome = "Flávia ";
+		String time = "Flamengo";
+		System.out.printf("Meu nome é %s\n",nome);
+		System.out.printf("Meu time é o %s",time);
+		
+	}
+
+}
